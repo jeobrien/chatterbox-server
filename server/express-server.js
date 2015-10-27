ef954c8810cf0501644c539da0ff32dd9f9cb023
jeobrien <j.e.obrien28@gmail.com>
2015-10-26 20:09:08 -0700

    Add client side, serving static files and started with json database

--- server/express-server.js
+++ server/express-server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var messages = [];
+// var html_dir = '../client/';
+var fs = require('fs');
+
+app.use(express.static('client'));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -15,7 +19,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json());
 
+// app.get('/', function (req, res) {
+//   res.sendFile('index.html', { root: 'client' });
+// });
+
+// app.get '/'
+  // res.send index.html
+
 app.get('/classes/messages', function (req, res) {
+  // fs.writeFile('./database.json', req.body, function() { console.log("Worked!"); });
   var reply = {
     results: messages
   };
@@ -23,9 +35,15 @@ app.get('/classes/messages', function (req, res) {
 });
 
 app.post('/classes/messages', function (req, res) {
-  console.log(req.body);
-  messages.push(req.body);
-  res.send();
+  fs.readFile('./server/database.json', function(err, data) {
+    console.log(data);
+    console.log(JSON.parse(data));
+    // data += JSON.stringify(req.body);
+    // fs.writeFile('./server/database.json', data, function() { console.log("Worked!"); });
+  });
+  // console.log(req.body);
+  // messages.push(req.body);
+  // res.send();
 });
 
 var server = app.listen(3000, function () {
